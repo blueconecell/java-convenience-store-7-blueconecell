277f9747ad151ea6f7063cf2c357f6121450ba9c
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-11-10 21:36:09 +0900

    feat(constant): 상수 선언
    - separator, resources 마크다운 파일 url 상수화

---
+++ src/main/java/store/constant/Resources.java
@@ -0,0 +1,15 @@
+package store.constant;
+
+public enum Resources {
+    PRODUCTS_URL("src/main/resources/products.md"),
+    PROMOTIONS_URL("src/main/resources/promotions.md");
+
+    private final String url;
+
+    Resources(String url) {
+        this.url = url;
+    }
+    public String getUrl() {
+        return url;
+    }
+}
