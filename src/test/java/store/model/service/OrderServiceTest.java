a504334d65910c44dcc01de3d34bc6656ecfb9d3
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2024-11-11 19:47:20 +0900

    chore: 우테코 과제제출 오류로 인해 커밋 재시도

---
+++ src/test/java/store/model/service/OrderServiceTest.java
@@ -0,0 +1,19 @@
+package store.model.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrderServiceTest {
+
+    @Test
+    public void 정상작동_테스트() throws Exception{
+        //given
+        
+        //when
+
+        //then
+    }
+
+
+}
